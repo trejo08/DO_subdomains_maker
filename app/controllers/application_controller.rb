0f710b1216ab8cfdd452562bd20a9a0118c0cd61
Trejo <trejocola@gmail.com>
2015-05-20 08:40:12 -0600

    agregando token de digitalOcean

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  require 'droplet_kit'
+  @token = 'e17e108a366c460bde1b877c81155df558c09ad732dbe8d762d3aa757c582864'
 end
